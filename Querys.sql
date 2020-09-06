2a90aab960158b3aaeb5ef7c056504d74e1e3244
Joshua Florence <joshuaflorence214@gmail.com>
2020-09-06 08:34:22 -0500

    Add files via upload

---
+++ Querys.sql
@@ -0,0 +1,80 @@
+--Number ONE
+SELECT employees.emp_no, 
+employees.last_name,
+employees.first_name,
+employees.sex,
+salaries.salary
+FROM employees
+LEFT JOIN salaries
+ON employees.emp_no = salaries.emp_no
+ORDER BY emp_no
+
+--Number TWO
+SELECT employees.first_name,
+employees.last_name,
+employees.hire_date
+FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY last_name;
+
+--NUMBER THREE
+SELECT dept_manager.dept_no, 
+departments.dept_name,
+dept_manager.emp_no,
+employees.last_name, 
+employees.first_name
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no
+ORDER BY emp_no
+
+--Number FOUR
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
+order by emp_no;
+
+--Number FIVE
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+--Number SIX
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
+
+-- Number SEVEN
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
+
+--Number EIGHT
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
